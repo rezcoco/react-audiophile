8fc6287cd19e3316368567f034158548d377f927
rezcoco <rezaainulzaki23@gmail.com>
2023-07-09 18:13:22 +0700

    fixup! fix msg validation layout

--- src/pages/Checkout/Form/Input/Input.jsx
+++ src/pages/Checkout/Form/Input/Input.jsx
@@ -22,7 +22,7 @@ const Input = ({ id, label, type, placeholder, className="" }) => {
 
   return (
     <div className={`${errors[id] ? 'sm-Input__grid__error xl-Input__grid__error' : 'sm-Input__grid xl-Input__grid'} ${className} xl:gap-2`}>
-      {errors[id] && (<p className="text-[13px] text-red-500 font-bold xl:col-start-2 xl:text-right mb-1 xl:mb-0">{errors[id].message}</p>) }
+      {errors[id] && (<p className="text-[13px] text-red-500 font-bold row-start-3 xl:row-start-1 xl:col-start-2 xl:text-right mt-1 xl:mt-0">{errors[id].message}</p>) }
       <label className={`text-xs font-bold row-start-1 ${errors[id] ? 'mb-[6px]' : 'mb-2'} xl:mb-0`} htmlFor={id}>{label}</label>
       <input
         {...register(id, {
